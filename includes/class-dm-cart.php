0f6638ba7f995e3a157ef4c792dee4345fd280a3
Deep Rahman <dp.rahman@gmail.com>
2023-01-19 22:49:58 +0600

    The cart class added

--- includes/class-dm-cart.php
+++ includes/class-dm-cart.php
@@ -21,24 +21,50 @@ if (!class_exists('DM_Cart')) :
         protected $cartUrl;
 
 
-        public static function init(array $params): self
+        public static function init($wc = WC(), $url = wc_get_cart_url(),array $params): self
         {
-            return new Self($params);
+            $params['cartUrl']= $url;
+            return new Self($wc, $params);
         }
 
-        public function __construct(array $params)
+        public function __construct(WooCommerce $wc,array $params)
         {
-            $this->woo = $params['wc'];
-            $this->cartUrl = $params['cartUrl'];
+            $this->woo = $wc;
+            $this->cartUrl = ($params['cartUrl'])?:'';
         }
 
-        protected function generateCartItemData():array
+        public function getCartItems():array{
+            return $this->generateCartItemData();
+        }
+
+        public function getCartUrl():string{
+            return $this->cartUrl;
+        }
+
+        public function getTotal():array{
+            return $this->cartTotalBreakUp();
+        }
+
+        /**
+         * returns cart url, cart total and item data
+         *
+         * @return array  ['cart_url' => `string`, 'cart_items' => `array`, 'cart_amount' => `array`]
+         */
+        public function getAllCartData():array{
+            return [
+                'cart_url' => $this->getCartUrl(),
+                'cart_items' => $this->getCartItems(),
+                'cart_amount' => $this->getTotal()
+            ];
+        }
+
+        protected function generateCartItemData(): array
         {
             $result = [];
             foreach ($this->woo->cart->get_cart() as $cart_item_key => $cart_item) {
                 $tmp = [];
                 $_product = $cart_item['data'];
-      
+
                 $tmp['permalink'] = $this->getProductPermalink($_product, $cart_item);
                 $tmp['removalLink'] = wc_get_cart_remove_url($cart_item_key);
                 $tmp['thumbnail'] = $_product->get_image();
@@ -47,7 +73,7 @@ if (!class_exists('DM_Cart')) :
                 $tmp['backOrderNotification'] = $this->backOrderNotification($_product, $cart_item);
                 $tmp['price'] = $this->woo->cart->get_product_price($_product);
                 $tmp['quantity'] = $this->$cart_item['quantity'];
-                $tmp['subtotal'] = $this->woo->cart->get_product_subtotal( $_product, $cart_item['quantity'] );
+                $tmp['subtotal'] = $this->woo->cart->get_product_subtotal($_product, $cart_item['quantity']);
                 $tmp['cartUrl'] = $this->cartUrl; // Send coupon code to this url
                 array_push($result, $tmp);
             }
@@ -70,9 +96,65 @@ if (!class_exists('DM_Cart')) :
             return '';
         }
 
-        protected function cartTotalBreakUp():array
+        protected function cartTotalBreakUp(): array
         {
-            
+            $ret = [];
+            $ret['subtotal'] = wc_cart_totals_subtotal_html();
+            $ret['coupons'] = $this->calculateForCoupons();
+            $ret['shippingInfo'] = $this->calculateForShipping();
+            $ret['taxDetails'] = $this->calculateForTax();
+            $ret['grossTotal'] = wc_cart_totals_order_total_html();
+            return $ret;
+        }
+
+        private function calculateForCoupons()
+        {
+            $coupons = [];
+            foreach ($this->woo->cart->get_coupons() as $code => $coupon) {
+                $coupons[$code] = wc_cart_totals_coupon_html($coupon);
+            }
+            return $coupons;
+        }
+
+        private function calculateForShipping(): string
+        {
+
+            if ($this->woo->cart->needs_shipping() && $this->woo->cart->show_shipping()) {
+                return wc_cart_totals_shipping_html();
+            } else if ($this->woo->cart->needs_shipping() && 'yes' === get_option('woocommerce_enable_shipping_calc')) {
+                return woocommerce_shipping_calculator();
+            } else {
+                return '';
+            }
+        }
+
+        private function calculateForTax(): array
+        {
+            $result = [];
+            if (wc_tax_enabled() && !$this->woo->cart->display_prices_including_tax()) {
+                $taxable_address = $this->woo->customer->get_taxable_address();
+                $estimated_text  = '';
+
+                if ($this->woo->customer->is_customer_outside_base() && !$this->woo->customer->has_calculated_shipping()) {
+                    /* translators: %s location. */
+                    $result['estimatedText'] = $estimated_text = sprintf(' <small>' . esc_html__('(estimated for %s)', 'woocommerce') . '</small>', $this->woo->countries->estimated_for_prefix($taxable_address[0]) . $this->woo->countries->countries[$taxable_address[0]]);
+                }
+
+                if ('itemized' === get_option('woocommerce_tax_total_display')) {
+                    $result['itemized'] = [];
+                    foreach ($this->woo->cart->get_tax_totals() as $code => $tax) {
+                        $tmp = [];
+                        $tmp['taxLabel'] = $tax->label;
+                        $tmp['taxAmount'] = $tax->formatted_amount;
+                        array_push($result['itemized'], $tmp);
+                    }
+                } else {
+
+                    $result['not_itemized'] = ['tax_label' => $this->woo->countries->tax_or_vat(), 'taxAmount' => wc_cart_totals_taxes_total_html()];
+                }
+            }
+
+            return $result;
         }
     }
 endif;
