977fa08e11f2be9942111c242dee557ab277a4b4
Your Name01~ <you@example.com>
2023-01-22 02:03:08 +0600

    payment section desing completed

--- includes/pages.php
+++ includes/pages.php
@@ -5,5 +5,6 @@
 return [
     'home',
     'shop',
-    'cart'
+    'cart',
+
 ];
--- includes/template-parts/cart/cart-items.htm.php
+++ includes/template-parts/cart/cart-items.htm.php
@@ -10,30 +10,18 @@
 
 <?php
 
-get_template_part('includes/template-parts/cart/cart-item-list.htm');
+get_template_part('includes/template-parts/cart/cart-interface.htm');
 wc_get_template('cart/cart-totals.php');
 ?>
 
 <div>
     <hr>
 </div>
-<section class="apply-coupon mt-3">
 
-    <form class="row g-3 justify-content-between">
-        <div class="col-auto">
-            <label for="coupon-code-input" class="visually-hidden">Email</label>
-            <input type="text" class="form-control" id="coupon-code-input" name="cart_coupon" placeholder="Enter Coupon Code">
-        </div>
-        <div class="col-auto">
-            <button type="submit" class="btn btn-outline-primary mb-3">Apply</button>
-        </div>
-    </form>
-
-</section>
 
 <section class="cart-item-help">
     <p class="lead">
         <a href="#" class="text-decoration-none"><?php esc_html_e('Is an item missing?', 'deamadre') ?></a>
-        <a href="#" class="text-decoration-none"><?php esc_html_e('Continue Shopping') ?></a>
+        <a href="<?php echo get_permalink(get_page_by_title( 'Shop' ));?>" class="text-decoration-none"><?php esc_html_e('Continue Shopping') ?></a>
     </p>
 </section>
